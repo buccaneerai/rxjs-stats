5dd5f9780b0f7f27d58ae290e9fbc486dd75e1b3
Brian <brianflaherty64@gmail.com>
2019-10-25 19:51:26 -0400

    operators: implemented api for operators module

---
+++ src/operators/index.js
@@ -0,0 +1,11 @@
+export { default as accuracy } from './accuracy';
+export { default as change } from './change';
+export { default as f1 } from './f1';
+export { default as mean } from './mean';
+export { default as precision } from './precision';
+export { default as recall } from './recall';
+export { default as roundTo } from './roundTo';
+// export { default as stdev } from './stdev';
+export { default as sum } from './sum';
+export { default as throwUnlessNum } from './throwUnlessNum';
+// export { default as variance } from './variance';
