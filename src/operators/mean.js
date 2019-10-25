f52bcf695335913ed8320e7cfd3b42d5df821b2a
Brian <brianflaherty64@gmail.com>
2019-10-24 22:20:31 -0400

    mean: tests now pass!

---
+++ src/operators/mean.js
@@ -0,0 +1,19 @@
+import { map, scan } from 'rxjs/operators';
+
+const reducer = function reducer([mean, sum, index], nextNum) {
+  return [
+    (sum + nextNum) / (index + 1),
+    sum + nextNum,
+    index + 1,
+  ];
+};
+
+// <T>(): MonoTypeOperatorFunction<T>
+const mean = function mean() {
+  return source$ => source$.pipe(
+    scan(reducer, [null, 0, 0]),
+    map(([average]) => average)
+  );
+};
+
+export default mean;
---
+++ src/operators/mean.test.js
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+import { from } from 'rxjs';
+import { marbles } from 'rxjs-marbles/mocha';
+
+import mean from './mean';
+
+describe('mean', () => {
+  it('should return the correct mean when given an observable of numbers', marbles(m => {
+    expect(1);
+    const num$ = m.cold('a--bc-d--e|', {a: 1, b: 2, c: 3, d: 4, e: 5});
+    const actual$ = num$.pipe(mean());
+    const expected$ = m.cold('a--bc-d--e|', {a: 1, b: 1.5, c: 2, d: 2.5, e: 3});
+    m.expect(actual$).toBeObservable(expected$);
+  }));
+});
