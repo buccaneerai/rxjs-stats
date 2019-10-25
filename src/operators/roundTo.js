831b5092bccf4e47ad0f1be0e1aef47abf8d5576
Brian <brianflaherty64@gmail.com>
2019-10-25 12:38:07 -0400

    roundTo: tests pass

---
+++ src/operators/roundTo.js
@@ -0,0 +1,29 @@
+import { map } from 'rxjs/operators';
+
+function round(number, method, precision) {
+  let _number = number;
+  if (!Number.isInteger(precision)) {
+    throw new Error('Expected precision to be an integer');
+  }
+
+  const isRoundingAndNegative = (method === 'round' && number < 0);
+  if (isRoundingAndNegative) _number = Math.abs(number);
+
+  let exponent;
+  [_number, exponent] = `${_number}e`.split('e');
+  let result = Math[method](`${_number}e${Number(exponent) + precision}`);
+
+  [_number, exponent] = `${result}e`.split('e');
+  result = Number(`${_number}e${Number(exponent) - precision}`);
+
+  if (isRoundingAndNegative) result = -result;
+  return result;
+}
+
+const roundTo = function roundTo(precision = 2) {
+  return source$ => source$.pipe(
+    map(num => round(num, precision, 'round'))
+  );
+};
+
+export default roundTo;
